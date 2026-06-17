89e3d30b46b6552bbfaca6716eff8bd7671d433d
zolbayar danzan <Zoloo@G303-CC0138.local>
2026-06-17 14:23:57 +0800

    connected to frontend

---
+++ web/app/admin/menu/page.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import axios from "axios";
+import { Badge } from "@/components/ui/badge";
+import { useEffect, useState } from "react";
+import { Skeleton } from "@/components/ui/skeleton";
+
+import AddCategoryDialog from "@/components/admin/components/AddCategoryDialog";
+
+type CategoryType = {
+  categoryName: string;
+  _id: string;
+};
+
+const Page = () => {
+  const [categories, setCategories] = useState<CategoryType[]>([]);
+  const [loading, setLoading] = useState(false);
+
+  const getCategories = async () => {
+    setLoading(true);
+    const response = await axios.get("http://localhost:3000/category");
+
+    console.log("working...", response);
+
+    setCategories(response.data.foodCategories);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getCategories();
+  }, []);
+
+  return (
+    <div className="h-screen w-full bg-secondary">
+      <div className="w-full rounded-xl p-6 space-y-4 bg-white">
+        <h3 className="text-xl font-semibold">Dishes category</h3>
+
+        <div className="flex gap-4">
+          {loading ? (
+            <>
+              <Skeleton className="h-8 w-40" />
+              <Skeleton className="h-8 w-40" />
+              <Skeleton className="h-8 w-40" />
+              <Skeleton className="h-8 w-40" />
+              <Skeleton className="h-8 w-40" />
+            </>
+          ) : (
+            categories.map((category) => {
+              return (
+                <div
+                  key={category._id}
+                  className="rounded-full py-2 px-4 border"
+                >
+                  {category.categoryName}
+                  <Badge className="ml-2">5</Badge>
+                </div>
+              );
+            })
+          )}
+
+          <AddCategoryDialog getCategories={getCategories} />
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Page;
---
+++ web/components/admin/components/AddCategoryDialog.tsx
@@ -0,0 +1,54 @@
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import axios from "axios";
+import { Plus } from "lucide-react";
+import { useState } from "react";
+
+const AddCategoryDialog = ({
+  getCategories,
+}: {
+  getCategories: () => void;
+}) => {
+  const [value, setValue] = useState("");
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  const addNewCategory = async () => {
+    await axios.post("http://localhost:3000/category", {
+      categoryName: value,
+    });
+
+    getCategories();
+  };
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button className="rounded-full bg-red-500 h-10">
+          <Plus />
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Add new category</DialogTitle>
+        </DialogHeader>
+        <h3>Category name</h3>
+        <Input onChange={handleChange} placeholder="type category name ..." />
+        <DialogClose asChild>
+          <Button onClick={addNewCategory}>Add category</Button>
+        </DialogClose>
+      </DialogContent>
+    </Dialog>
+  );
+};
+export default AddCategoryDialog;
